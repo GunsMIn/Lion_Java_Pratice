18a023213c0b2455675d1dbb072b0145c7036893
KimGunWoo9595 <medneter@naver.com>
2022-10-04 15:32:38 +0900

    짝수 홀수문제

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public String test(int num) {
+        String answer = "";
+        if(num%2 == 0){
+            answer = "even";
+        }else{
+            answer= "odd";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        main.test(num);
+
+    }
+}
