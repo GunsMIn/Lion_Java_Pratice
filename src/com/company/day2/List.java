991ffb99b497662b4b70a781f7ac069b7c95beb3
KimGunWoo9595 <medneter@naver.com>
2022-10-05 15:31:18 +0900

    List

---
+++ src/com/company/day2/List.java
@@ -0,0 +1,22 @@
+package com.company.day2;
+
+import javax.naming.Name;
+import java.util.ArrayList;
+
+public class List {
+    java.util.List<String> names = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Names names = new Names();
+        System.out.println(names.getList());
+        System.out.println(names.getList().size()+"명");
+
+
+
+
+
+    }
+
+
+}
---
+++ src/com/company/day2/Names.java
@@ -0,0 +1,58 @@
+package com.company.day2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Names {
+
+    List<String> names = new ArrayList<>();
+
+    public List<String> getList() {
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        names.add("김xx");
+        names.add("박xx");
+        names.add("최xx");
+        names.add("이xx");
+        return names;
+    }
+
+}
