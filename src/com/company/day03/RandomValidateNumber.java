f5a32cd376f31a9098b5675dfa2d53c58071904f
KimGunWoo9595 <medneter@naver.com>
2022-10-06 10:17:25 +0900

    중복제거

--- src/com/company/day03/RandomValidateNumber.java
+++ src/com/company/day03/RandomValidateNumber.java
@@ -8,11 +8,11 @@ public class RandomValidateNumber {
 
     public static void main(String[] args) {
         RandomNumberCreator randomNumberGenerator = new RandomNumberCreator();
-        Set<Integer> setInteger = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < 50; i++) {
-            setInteger.add(randomNumberGenerator.generate(50));
+            set.add(randomNumberGenerator.generate(50));
         }
-        System.out.println("Set 개수 : " + setInteger.size());
-        System.out.println(setInteger);
+        System.out.println("중복제거된 count: " + set.size());
+        System.out.println(set);
     }
 }
