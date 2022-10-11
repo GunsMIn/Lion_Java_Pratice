f830690e83a31feeb446779e520543528542f96b
KimGunWoo9595 <medneter@naver.com>
2022-10-11 09:36:28 +0900

    강사님 자릿수 더하기 알고리즘

---
+++ src/com/company/day05/TeacherMain.java
@@ -0,0 +1,24 @@
+package com.company.day05;
+
+import java.util.Scanner;
+
+public class TeacherMain {
+    public int solution(int n) {
+        int result = 0;
+        int origin = n;
+        while (origin > 0) {
+            result += origin % 10; // 나머지를 먼저 구할 것
+            origin = origin / 10;
+            System.out.printf("origin : %d result : %d\n",origin,result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        TeacherMain m = new TeacherMain();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(m.solution(n));
+    }
+}
