b12ba358bcb7fd163b5bbe2bfeef763a70845b6a
KimGunWoo9595 <medneter@naver.com>
2022-10-13 10:17:29 +0900

    선택정렬

--- src/com/company/day07/SelectionSort.java
+++ src/com/company/day07/SelectionSort.java
@@ -4,18 +4,25 @@ import java.util.Arrays;
 
 public class SelectionSort {
 
+    //선택정렬은 가장 작은것을 선택해서 가장 앞으로 보낸다.
     public static void main(String[] args) {
-        int[] a = {7, 2, 9, 20, 11};
-        int tmp = a[0];
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i+1; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    tmp = a[i];
-                    a[i] = a[j];
-                    a[j] = tmp;
-                }
+
+        int[] arr = {7, 2, 9, 20, 11};
+        int tmp = 0;
+
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min])
+                    min = j;
             }
+            tmp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = tmp;
         }
-        System.out.println(Arrays.toString(a));
+
+        System.out.println(Arrays.toString(arr));
     }
-}
+    }
+
