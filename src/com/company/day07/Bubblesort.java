aa25dd58cb26497c3d48a48f09ddb5bbc205c37a
KimGunWoo9595 <medneter@naver.com>
2022-10-14 14:21:54 +0900

    선택정렬과 버블정렬

--- src/com/company/day07/Bubblesort.java
+++ src/com/company/day07/Bubblesort.java
@@ -6,21 +6,22 @@ public class Bubblesort {
     
     public static void main(String[] args) {
 
-        int[] a = {7, 2, 9, 20, 11};
-        		int tmp;
-        		for(int i = 0 ; i < a.length ; i ++) {
-        			for(int j = 0 ; j < a.length -i -1 ; j ++) {
-        				if(a[j]>a[j+1]) {
-                            tmp = a[j];
-        					a[j] = a[j+1];
-        					a[j+1] = tmp;
+        		int[] a = {7, 2, 9, 20, 11};
+        		int tmp; // 2개의 원소를 바꾸기윈한 배열을 담을 수 있는 저장소소
+
+       			for(int i = 0 ; i < a.length ; i ++) {
+        			for(int j = 0 ; j < a.length -i -1 ; j ++) { // 버블 정렬은 집합을 감소시키는 특성을 가지고있다.
+        				if(a[j]>a[j+1]) { // 당장 바로 옆에있는것과 비교하는것이다.
+							tmp = a[j];
+							a[j] = a[j+1];
+							a[j + 1] = tmp;
+        					}
         				}
         			}
-        		}
 
-        		for(int i = 0 ; i < a.length ; i ++) {
-        			System.out.println(a[i]);
-        		}
+        			for(int i = 0 ; i < a.length ; i ++) {
+        				System.out.println(a[i]);
+        			}
 
     }
 
--- src/com/company/day07/SelectionSort.java
+++ src/com/company/day07/SelectionSort.java
@@ -10,18 +10,16 @@ public class SelectionSort {
         int[] arr = {7, 2, 9, 20, 11};
         int tmp = 0;
 
-
         for (int i = 0; i < arr.length - 1; i++) {
-            int min = i;
+            int min = i; // 선택정렬에서의 최소값의 인덱스번호
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[min])
                     min = j;
             }
-            tmp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = tmp;
+            tmp = arr[min]; // 가장 최소값을 저장소에 저장
+            arr[min] = arr[i]; // i의 인덱스 배열이랑 최소값 배열값 교환
+            arr[i] = tmp;   //최소값을 i의 인덱스에 저장
         }
-
         System.out.println(Arrays.toString(arr));
     }
     }
